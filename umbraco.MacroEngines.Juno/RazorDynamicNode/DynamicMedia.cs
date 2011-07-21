144adece331ee5e32192f513db5a1f77dbcaaec1
sgay <sgay@localhost>
2011-07-21 18:48:39 +0200

    Fix member cookie value encoding [#30350]

---
+++ umbraco.MacroEngines.Juno/RazorDynamicNode/DynamicMedia.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace umbraco.MacroEngines
+{
+    //for backwards compatibility only
+    public class DynamicMedia : DynamicNode
+    {
+        public DynamicMedia(DynamicBackingItem item) : base(item) { }
+    }
+}
