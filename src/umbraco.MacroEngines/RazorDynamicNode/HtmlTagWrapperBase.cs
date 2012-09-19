2280221920061341c5205902007dea3e9eb5a1a8
Casey Neehouse <casey@umbraco.com>
2012-09-19 07:02:02 -0400

    Removed umbracoApp and umbracoAppTree Foreign Keys from SqlAzure install script.

---
+++ src/umbraco.MacroEngines/RazorDynamicNode/HtmlTagWrapperBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.UI;
+using System.Web;
+
+namespace umbraco.MacroEngines
+{
+
+	[Obsolete("This interface has been superceded by Umbraco.Core.IHtmlTagWrapper")]	
+    public interface HtmlTagWrapperBase
+    {
+        void WriteToHtmlTextWriter(HtmlTextWriter html);
+    }
+}
