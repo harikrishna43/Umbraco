14bf82600b906be4199b37bd8aec17fe0f3dc304
Shannon Deminick <sdeminick@gmail.com>
2012-09-21 09:54:14 -0700

    Made IThumbnailProvider public which fixes 4.10 loading them, plus now people can make their own :) However, I'd like to move the Priority flag onto an attribute instead of part of the interface because it is metadata and not functionality of the class itself. Will make a tracking issue for it.

---
+++ src/Umbraco.Core/DocumentExtensions.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using Umbraco.Core.Models;
+using umbraco.interfaces;
+
+namespace Umbraco.Core
+{
+	public static class DocumentExtensions
+	{
+		/// <summary>
+		/// Returns the property as the specified type, if the property is not found or does not convert
+		/// then the default value of type T is returned.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="prop"></param>
+		/// <param name="alias"></param>
+		/// <returns></returns>
+		public static T GetPropertyValue<T>(this IDocument prop, string alias)
+		{
+			return prop.GetPropertyValue<T>(alias, default(T));
+		}
+
+		public static T GetPropertyValue<T>(this IDocument prop, string alias, T ifCannotConvert)
+		{
+			var p = prop.GetProperty(alias);
+			if (p == null)
+				return default(T);
+			var converted = p.TryConvertTo<T>();
+			if (converted.Success)
+				return converted.Result;
+			return ifCannotConvert;
+		}
+		
+	}
+}
---
+++ src/Umbraco.Core/Dynamics/DynamicDocumentIdEqualityComparer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Umbraco.Core.Dynamics
+{
+    internal class DynamicDocumentIdEqualityComparer : EqualityComparer<DynamicDocument>
+    {
+
+        public override bool Equals(DynamicDocument x, DynamicDocument y)
+        {
+            //Check whether the compared objects reference the same data.
+            if (Object.ReferenceEquals(x, y)) return true;
+
+            //Check whether any of the compared objects is null.
+            if (Object.ReferenceEquals(x, null) || Object.ReferenceEquals(y, null))
+                return false;
+
+            //Check whether the nodes ids are equal.
+            return x.Id == y.Id;
+
+        }
+
+        public override int GetHashCode(DynamicDocument obj)
+        {
+            if (Object.ReferenceEquals(obj, null)) return 0;
+
+            //Get hash code for the Name field if it is not null.
+            int hashId = obj.Id.GetHashCode();
+
+            return hashId;
+        }
+
+    }
+}
