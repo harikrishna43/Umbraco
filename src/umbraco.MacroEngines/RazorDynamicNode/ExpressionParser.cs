e2e912f3da48f968d3cc99e88d5b097ea03d7216
Stephan <sgay@pilotine.com>
2012-10-06 11:20:12 +0200

    fix config files for BaseRestExtension.config

---
+++ src/umbraco.MacroEngines/RazorDynamicNode/ExpressionParser.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq.Expressions;
+using System.Reflection;
+using umbraco.MacroEngines;
+
+namespace System.Linq.Dynamic
+{
+	internal class ExpressionParser : Umbraco.Web.Dynamics.ExpressionParser
+	{
+		public ExpressionParser(ParameterExpression[] parameters, string expression, object[] values)
+			: base(parameters, expression, values)
+		{
+		}
+	}
+
+
+}
