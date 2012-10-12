3132f6c159f2a8d600355e2062ccec7331612a95
Morten <Morten@Thinkpad-X220>
2012-10-12 16:37:55 +0200

    Code from freedom friday project extending petapoco with a bunch of annotations used to decorate strongly typed models for table creation

---
+++ src/Umbraco.Core/Models/Rdbms/ContentDto.cs
@@ -0,0 +1,32 @@
+﻿using Umbraco.Core.Persistence;
+using Umbraco.Core.Persistence.DatabaseAnnotations;
+
+namespace Umbraco.Core.Models.Rdbms
+{
+    [TableName("cmsContent")]
+    [PrimaryKey("pk")]
+    [ExplicitColumns]
+    internal class ContentDto
+    {
+        [Column("pk")]
+        [PrimaryKeyColumn]
+        [DatabaseType(DatabaseTypes.Integer)]
+        [NullSetting(NullSetting = NullSettings.Null)]
+        public int PrimaryKey { get; set; }
+
+        [Column("nodeId")]
+        [ForeignKey(typeof(NodeDto))]
+        [DatabaseType(DatabaseTypes.Integer)]
+        [NullSetting(NullSetting = NullSettings.Null)]
+        [Index(IndexTypes.UniqueNonclustered, Name = "IX_cmsContent")]
+        public int NodeId { get; set; }
+
+        [Column("contentType")]
+        [DatabaseType(DatabaseTypes.Integer)]
+        [NullSetting(NullSetting = NullSettings.Null)]
+        public int ContentType { get; set; }
+
+        [ResultColumn]
+        public NodeDto NodeDto { get; set; }
+    }
+}
