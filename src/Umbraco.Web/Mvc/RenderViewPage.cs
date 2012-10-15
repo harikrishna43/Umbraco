c5d43e5720f858fca4fe28663267e7280418c8a9
Shannon Deminick <sdeminick@gmail.com>
2012-10-15 15:33:35 -0500

    Initial commit of refactoring of package installer for the installer process. Converted a few webforms pages/controls to the new format, created new JS class to manage the installation, created new MVC controller for managing the ajax installation

---
+++ src/Umbraco.Web/Mvc/RenderViewPage.cs
@@ -0,0 +1,67 @@
+using System.Web.Mvc;
+using Umbraco.Core;
+using Umbraco.Core.Dictionary;
+using Umbraco.Core.Dynamics;
+using Umbraco.Core.Models;
+using Umbraco.Web.Models;
+using Umbraco.Web.Routing;
+
+namespace Umbraco.Web.Mvc
+{
+	/// <summary>
+	/// The View that front-end templates inherit from
+	/// </summary>
+	public abstract class RenderViewPage : WebViewPage<RenderModel>
+	{
+		protected RenderViewPage()
+		{
+
+		}
+
+		protected override void InitializePage()
+		{
+			//set the model to the current node if it is not set, this is generally not the case
+			if (Model != null)
+			{
+				////this.ViewData.Model = Model;
+				//var backingItem = new DynamicBackingItem(Model.CurrentNode);
+				var dynamicNode = new DynamicPublishedContent(Model.Content);
+				CurrentPage = dynamicNode.AsDynamic();
+			}
+			PublishedContentRequest = (PublishedContentRequest)ViewContext.RouteData.DataTokens.GetRequiredObject("umbraco-doc-request");
+			UmbracoContext = (UmbracoContext)ViewContext.RouteData.DataTokens.GetRequiredObject("umbraco-context");
+			ApplicationContext = UmbracoContext.Application;
+		}
+
+		/// <summary>
+		/// Returns the current UmbracoContext
+		/// </summary>
+		public UmbracoContext UmbracoContext { get; private set; }
+
+		/// <summary>
+		/// Returns the current ApplicationContext
+		/// </summary>
+		public ApplicationContext ApplicationContext { get; private set; }
+
+		/// <summary>
+		/// Returns the current PublishedContentRequest
+		/// </summary>
+		internal PublishedContentRequest PublishedContentRequest { get; private set; }
+
+		/// <summary>
+		/// Returns the a DynamicPublishedContent object
+		/// </summary>
+		public dynamic CurrentPage { get; private set; }		
+
+		private UmbracoHelper _helper;
+
+		/// <summary>
+		/// Gets an UmbracoHelper
+		/// </summary>
+		public UmbracoHelper Umbraco
+		{
+			get { return _helper ?? (_helper = new UmbracoHelper(UmbracoContext)); }
+		}
+
+	}
+}
---
+++ src/umbraco.datalayer/SqlHelpers/SqlServer/SqlServerInstaller.cs
@@ -0,0 +1,106 @@
+/************************************************************************************
+ * 
+ *  Umbraco Data Layer
+ *  MIT Licensed work
+ *  ©2008 Ruben Verborgh
+ * 
+ ***********************************************************************************/
+
+using System;
+using System.Resources;
+using umbraco.DataLayer.Utility.Installer;
+
+namespace umbraco.DataLayer.SqlHelpers.SqlServer
+{
+    /// <summary>
+    /// Database installer for an SQL Server data source.
+    /// </summary>
+    public class SqlServerInstaller : DefaultInstallerUtility<SqlServerHelper>
+    {
+        #region Private Constants
+       
+        /// <summary>The latest database version this installer supports.</summary>
+        private const DatabaseVersion LatestVersionSupported = DatabaseVersion.Version4_9;
+
+        /// <summary>The specifications to determine the database version.</summary>
+        private static readonly VersionSpecs[] m_VersionSpecs = new VersionSpecs[] {
+					new VersionSpecs("SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'cmsContentType2ContentType'", 1, DatabaseVersion.Version4_9), 
+					//This will throw an exception and will only ever have one row if it is installed, on exception the database version returned is unkown so will still work.
+					//NOTE: before this was set to zero and only worked by fluke!
+					new VersionSpecs("SELECT CONSTRAINT_NAME FROM INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS LEFT OUTER JOIN umbracoApp ON appAlias = appAlias WHERE CONSTRAINT_NAME = 'FK_umbracoUser2app_umbracoApp'", 1, DatabaseVersion.Version4_8), 
+					new VersionSpecs("SELECT id FROM umbracoNode WHERE id = -21", 1, DatabaseVersion.Version4_1),        
+					new VersionSpecs("SELECT action FROM umbracoAppTree", DatabaseVersion.Version4),
+                    new VersionSpecs("SELECT description FROM cmsContentType", DatabaseVersion.Version3),
+                    new VersionSpecs("SELECT id FROM sysobjects", DatabaseVersion.None) };
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// Gets a value indicating whether the installer can upgrade the data source.
+        /// </summary>
+        /// <value>
+        /// 	<c>true</c> if the installer can upgrade the data source; otherwise, <c>false</c>.
+        /// </value>
+        /// <remarks>Empty data sources can't be upgraded, just installed.</remarks>
+        public override bool CanUpgrade
+        {
+            get
+            {
+                return CurrentVersion == DatabaseVersion.Version4_1;
+            }
+        }
+
+        #endregion
+
+        #region Protected Properties
+
+        /// <summary>
+        /// Gets the version specification for evaluation by DetermineCurrentVersion.
+        /// Only first matching specification is taken into account.
+        /// </summary>
+        /// <value>The version specifications.</value>
+        protected override VersionSpecs[] VersionSpecs
+        {
+            get { return m_VersionSpecs; }
+        }
+
+        #endregion
+
+        #region Public Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SqlServerInstaller"/> class.
+        /// </summary>
+        /// <param name="sqlHelper">The SQL helper.</param>
+        public SqlServerInstaller(SqlServerHelper sqlHelper) : base(sqlHelper, LatestVersionSupported)
+        { }
+
+        #endregion
+
+        #region DefaultInstaller Members       
+
+		/// <summary>
+		/// Returns the sql to do a full install
+		/// </summary>
+		protected override string FullInstallSql
+		{
+			get { return SqlResources.Total; }
+		}
+
+		/// <summary>
+		/// Returns the sql to do an upgrade
+		/// </summary>
+		protected override string UpgradeSql
+		{
+			get
+			{
+				string upgradeFile = string.Format("{0}_Upgrade", CurrentVersion.ToString());
+				return SqlResources.ResourceManager.GetString(upgradeFile);
+			}
+		}		
+
+        #endregion
+    }
+}
