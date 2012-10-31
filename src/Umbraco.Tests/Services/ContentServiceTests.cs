228fc253dae6573b81fe0b7c84429b249bdb7cb0
Sebastiaan Janssen <sebastiaan@umbraco.com>
2012-10-31 17:31:52 +0100

    NuGet Package restore problem seems related to version of nuget.exe, updated See: http://nuget.codeplex.com/discussions/398910

---
+++ src/Umbraco.Tests/Services/ContentServiceTests.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Umbraco.Core.Models;
+using Umbraco.Tests.TestHelpers;
+using Umbraco.Tests.TestHelpers.Entities;
+
+namespace Umbraco.Tests.Services
+{
+    [TestFixture]
+    public class ContentServiceTests : BaseDatabaseFactoryTest
+    {
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            CreateTestData();
+        }
+
+        [Test]
+        public void Can_Create_Content()
+        {
+            // Arrange
+            var contentService = ServiceContext.ContentService;
+
+            // Act
+            IContent content = contentService.CreateContent(-1, "umbTextpage");
+
+            // Assert
+            Assert.That(content, Is.Not.Null);
+            Assert.That(content.HasIdentity, Is.False);
+        }
+
+        [Test]
+        public void Cannot_Create_Content_With_Non_Existing_ContentType_Alias()
+        {
+            // Arrange
+            var contentService = ServiceContext.ContentService;
+
+            // Act & Assert
+            Assert.Throws<Exception>(() => contentService.CreateContent(-1, "umbAliasDoesntExist"));
+        }
+
+        [Test]
+        public void Can_Get_Content_By_Id()
+        {
+            // Arrange
+            var contentService = ServiceContext.ContentService;
+
+            // Act
+            var content = contentService.GetById(1046);
+
+            // Assert
+            Assert.That(content, Is.Not.Null);
+            Assert.That(content.Id, Is.EqualTo(1046));
+        }
+
+        [Test]
+        public void Can_Get_Content_By_Level()
+        {
+            // Arrange
+            var contentService = ServiceContext.ContentService;
+
+            // Act
+            var contents = contentService.GetByLevel(2);
+
+            // Assert
+            Assert.That(contents, Is.Not.Null);
+            Assert.That(contents.Any(), Is.True);
+            Assert.That(contents.Count(), Is.GreaterThanOrEqualTo(2));
+        }
+
+        [Test]
+        public void Can_Get_Children_Of_Content_Id()
+        {
+            // Arrange
+            var contentService = ServiceContext.ContentService;
+
+            // Act
+            var contents = contentService.GetChildren(1046);
+
+            // Assert
+            Assert.That(contents, Is.Not.Null);
+            Assert.That(contents.Any(), Is.True);
+            Assert.That(contents.Count(), Is.GreaterThanOrEqualTo(2));
+        }
+
+        public void Can_Get_All_Versions_Of_Content()
+        { }
+
+        public void Can_Get_Root_Content()
+        { }
+
+        public void Can_Get_Content_For_Expiration()
+        { }
+
+        public void Can_Get_Content_For_Release()
+        { }
+
+        public void Can_Get_Content_In_RecycleBin()
+        { }
+
+        public void Can_RePublish_All_Content()
+        { }
+
+        public void Can_Publish_Content()
+        { }
+
+        public void Can_Publish_Content_Children()
+        { }
+
+        public void Can_Save_And_Publish_Content()
+        { }
+
+        public void Can_Save_Content()
+        { }
+
+        public void Can_Bulk_Save_Content()
+        { }
+
+        public void Can_Delete_Content_Of_Specific_ContentType()
+        { }
+
+        public void Can_Delete_Content()
+        { }
+
+        public void Can_Move_Content_To_RecycleBin()
+        { }
+
+        public void Can_Move_Content()
+        { }
+
+        public void Can_Copy_Content()
+        { }
+
+        public void Can_Send_To_Publication()
+        { }
+
+        public void Can_Rollback_Version_On_Content()
+        { }
+
+        public void CreateTestData()
+        {
+            //NOTE Maybe not the best way to create/save test data as we are using the services, which are being tested.
+
+            //Create and Save ContentType "umbTextpage" -> 1045
+            ContentType contentType = MockedContentTypes.CreateSimpleContentType("umbTextpage", "Textpage");
+            ServiceContext.ContentTypeService.Save(contentType);
+
+            //Create and Save Content "Homepage" based on "umbTextpage" -> 1046
+            Content textpage = MockedContent.CreateTextpageContent(contentType);
+            ServiceContext.ContentService.Save(textpage, 0);
+
+            //Create and Save Content "Text Page 1" based on "umbTextpage" -> 1047
+            Content subpage = MockedContent.CreateTextpageContent(contentType, "Text Page 1", textpage.Id);
+            ServiceContext.ContentService.Save(subpage, 0);
+
+            //Create and Save Content "Text Page 1" based on "umbTextpage" -> 1048
+            Content subpage2 = MockedContent.CreateTextpageContent(contentType, "Text Page 2", textpage.Id);
+            ServiceContext.ContentService.Save(subpage2, 0);
+        }
+    }
+}
