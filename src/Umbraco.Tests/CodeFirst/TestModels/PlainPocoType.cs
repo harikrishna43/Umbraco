7c9015dbcc81aac85ffd23e8b26846c1bdf937e9
Morten Christensen <Morten Christensen@localhost>
2012-11-26 16:13:50 +0100

    Fixing an issue I had introduced in ExpressionHelper

---
+++ src/Umbraco.Tests/CodeFirst/TestModels/PlainPocoType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Umbraco.Tests.CodeFirst.TestModels
+{
+    //Plain Poco Type - plainPocoType
+    public class PlainPocoType
+    {
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public bool IsFinished { get; set; }
+
+        public int Weight { get; set; }
+
+        public DateTime PublishDate { get; set; }
+    }
+}
