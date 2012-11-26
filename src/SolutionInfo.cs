1a36a892273a26aee9bf4681f16c18e191ebd3e9
Sebastiaan Janssen <sebastiaan@umbraco.com>
2012-11-26 14:07:16 +0100

    Don't generate documentation file in release mode

---
+++ src/SolutionInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security;
+
+//This is the core assembly information for the whole project
+[assembly: AssemblyVersion("6.0.0.0")]
+[assembly: AssemblyFileVersion("6.0.0.0")]
+[assembly: AssemblyInformationalVersion("Umbraco 6.0")]
+[assembly: AssemblyCompany("Umbraco")]
+[assembly: AssemblyCopyright("Copyright © 2012")]
+[assembly: AssemblyTrademark("Umbraco")]
+[assembly: AssemblyCulture("")]
