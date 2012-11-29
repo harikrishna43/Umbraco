f3ae6759f397095ab10e466bacea6f1fcacc879e
Sebastiaan Janssen <sebastiaan@umbraco.com>
2012-11-29 17:38:04 +0100

    Reimplement the changed DB schema and adjust the code to work with it

---
+++ src/Umbraco.Tests/CodeFirst/TestModels/Composition/Meta.cs
@@ -0,0 +1,13 @@
+﻿using Umbraco.Tests.CodeFirst.Attributes;
+
+namespace Umbraco.Tests.CodeFirst.TestModels.Composition
+{
+    public class Meta : IMeta
+    {
+         
+    }
+
+    [Alias("meta", Name = "Meta")]
+    public interface IMeta
+    {}
+}
