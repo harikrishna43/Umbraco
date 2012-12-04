db0ef825a5dee4ed5d6803e735046bde31e079f8
Morten Christensen <Morten Christensen@localhost>
2012-12-04 09:36:51 +0100

    Creating a new DefinitionFactory for refactoring the database definitions used by the syntax provider.

---
+++ src/Umbraco.Core/Persistence/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using Umbraco.Core.Models.EntityBase;
+
+namespace Umbraco.Core.Persistence.UnitOfWork
+{
+    /// <summary>
+    /// Defines a Unit Of Work
+    /// </summary>
+    public interface IUnitOfWork
+    {
+        void RegisterAdded(IEntity entity, IUnitOfWorkRepository repository);
+        void RegisterChanged(IEntity entity, IUnitOfWorkRepository repository);
+        void RegisterRemoved(IEntity entity, IUnitOfWorkRepository repository);
+        void Commit();
+        object Key { get; }
+    }
+}
