ea8016fde1d54a30a48869efe1ed20815e95fc55
Shannon Deminick <sdeminick@gmail.com>
2012-12-13 23:11:53 -0500

    Fixes: #U4-1272 which now enables the hidden feature of razor macros (this feature also works in MVC)

---
+++ src/Umbraco.Core/Persistence/Migrations/Syntax/Rename/RenameBuilder.cs
@@ -0,0 +1,30 @@
+﻿using Umbraco.Core.Persistence.Migrations.Syntax.Rename.Column;
+using Umbraco.Core.Persistence.Migrations.Syntax.Rename.Expressions;
+using Umbraco.Core.Persistence.Migrations.Syntax.Rename.Table;
+
+namespace Umbraco.Core.Persistence.Migrations.Syntax.Rename
+{
+    public class RenameBuilder : IRenameBuilder
+    {
+        private readonly IMigrationContext _context;
+
+        public RenameBuilder(IMigrationContext context)
+        {
+            _context = context;
+        }
+
+        public IRenameTableSyntax Table(string oldName)
+        {
+            var expression = new RenameTableExpression { OldName = oldName };
+            _context.Expressions.Add(expression);
+            return new RenameTableBuilder(expression);
+        }
+
+        public IRenameColumnTableSyntax Column(string oldName)
+        {
+            var expression = new RenameColumnExpression { OldName = oldName };
+            _context.Expressions.Add(expression);
+            return new RenameColumnBuilder(expression);
+        }
+    }
+}
