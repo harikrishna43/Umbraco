22c213e7b5f4ccf0e68c09b2c9b8838cdce095d6
Sebastiaan Janssen <sebastiaan@umbraco.com>
2013-01-03 15:56:53 +0100

    Fix missing connection string during upgrades

---
+++ src/Umbraco.Core/Models/IContentTypeBase.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using Umbraco.Core.Models.EntityBase;
+
+namespace Umbraco.Core.Models
+{
+    /// <summary>
+    /// Defines the base for a ContentType with properties that
+    /// are shared between ContentTypes and MediaTypes.
+    /// </summary>
+    public interface IContentTypeBase : IUmbracoEntity
+    {
+        /// <summary>
+        /// Gets or Sets the Alias of the ContentType
+        /// </summary>
+        string Alias { get; set; }
+
+        /// <summary>
+        /// Gets or Sets the Name of the ContentType
+        /// </summary>
+        string Name { get; set; }
+
+        /// <summary>
+        /// Gets or Sets the Description for the ContentType
+        /// </summary>
+        string Description { get; set; }
+
+        /// <summary>
+        /// Gets or Sets the Icon for the ContentType
+        /// </summary>
+        string Icon { get; set; }
+
+        /// <summary>
+        /// Gets or Sets the Thumbnail for the ContentType
+        /// </summary>
+        string Thumbnail { get; set; }
+
+        /// <summary>
+        /// Gets or Sets a boolean indicating whether this ContentType is allowed at the root
+        /// </summary>
+        bool AllowedAsRoot { get; set; }
+
+        /// <summary>
+        /// Gets or Sets a boolean indicating whether this ContentType is a Container
+        /// </summary>
+        /// <remarks>
+        /// ContentType Containers doesn't show children in the tree, but rather in grid-type view.
+        /// </remarks>
+        bool IsContainer { get; set; }
+
+        /// <summary>
+        /// Gets or Sets a list of integer Ids of the ContentTypes allowed under the ContentType
+        /// </summary>
+        IEnumerable<ContentTypeSort> AllowedContentTypes { get; set; }
+
+        /// <summary>
+        /// Gets or Sets a collection of Property Groups
+        /// </summary>
+        PropertyGroupCollection PropertyGroups { get; set; }
+
+        /// <summary>
+        /// Gets an enumerable list of Property Types aggregated for all groups
+        /// </summary>
+        IEnumerable<PropertyType> PropertyTypes { get; }
+
+        void SetLazyParentId(Lazy<int> id);
+    }
+}
