41abdff7f7243238615e19b9b3fddffe7d85524c
Sebastiaan Janssen <sebastiaan@umbraco.com>
2013-01-03 17:20:13 +0100

    U4-335 Fixed out of memory exception in cropper (checks in CropImage method)

---
+++ src/Umbraco.Web/umbraco.presentation/install/steps/Definitions/Database.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Umbraco.Core.Configuration;
+using umbraco.cms.businesslogic.installer;
+using umbraco.IO;
+
+namespace umbraco.presentation.install.steps.Definitions
+{
+    public class Database : InstallerStep
+    {
+        public override string Alias
+        {
+            get { return "database"; }
+        }
+
+        public override string Name
+        {
+          get { return "Database"; }
+        }
+
+        public override string UserControl
+        {
+            get { return SystemDirectories.Install + "/steps/database.ascx"; }
+        }
+
+        
+        public override bool MoveToNextStepAutomaticly
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        //here we determine if the installer should skip this step...
+        public override bool Completed()
+        {
+            bool retval = false;
+            try
+            {
+                var configuredVersion = new Version(Umbraco.Core.Configuration.GlobalSettings.ConfigurationStatus);
+                var targetVersion = UmbracoVersion.Current;
+
+                retval = targetVersion > configuredVersion;
+
+            } catch {
+                // this step might fail due to missing connectionstring
+                return false;
+            }
+
+            return retval;
+        }
+
+        
+    }
+}
