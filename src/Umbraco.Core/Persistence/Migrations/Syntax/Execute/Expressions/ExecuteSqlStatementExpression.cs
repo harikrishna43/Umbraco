957078932821030e1193e0ca34377693d0d1cf72
Sebastiaan Janssen <sebastiaan@umbraco.com>
2013-01-03 10:19:04 +0100

    Check if the propertyNode.FirstChild.Value is in content.Properties before trying to get it

---
+++ src/Umbraco.Core/Persistence/Migrations/Syntax/Execute/Expressions/ExecuteSqlStatementExpression.cs
@@ -0,0 +1,20 @@
+﻿namespace Umbraco.Core.Persistence.Migrations.Syntax.Execute.Expressions
+{
+    public class ExecuteSqlStatementExpression : MigrationExpressionBase
+    {
+        public ExecuteSqlStatementExpression()
+        {
+        }
+
+        public ExecuteSqlStatementExpression(DatabaseProviders current, DatabaseProviders[] databaseProviders) : base(current, databaseProviders)
+        {
+        }
+
+        public virtual string SqlStatement { get; set; }
+
+        public override string ToString()
+        {
+            return SqlStatement;
+        }
+    }
+}
