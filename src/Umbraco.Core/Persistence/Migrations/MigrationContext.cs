c6049e9098d9ec69d0acdf9dbbb675f6876eac35
Sebastiaan Janssen <sebastiaan@umbraco.com>
2013-01-03 13:21:23 +0100

    Remove deleted images from the solution as well

---
+++ src/Umbraco.Core/Persistence/Migrations/MigrationContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Umbraco.Core.Persistence.Migrations
+{
+    internal class MigrationContext : IMigrationContext
+    {
+        public MigrationContext(DatabaseProviders databaseProvider)
+        {
+            Expressions = new Collection<IMigrationExpression>();
+            CurrentDatabaseProvider = databaseProvider;
+        }
+
+        public ICollection<IMigrationExpression> Expressions { get; set; }
+
+        public DatabaseProviders CurrentDatabaseProvider { get; private set; }
+    }
+}
