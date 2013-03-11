0d30dee1dcfbbc0ce14382d24b2bb3198e15b7bb
Shannon Deminick <sdeminick@gmail.com>
2013-03-11 11:46:47 -0600

    Fixes: #U4-1890 - allows access to the UmbracoContext during application startup.

---
+++ src/Umbraco.Core/Models/IMediaType.cs
@@ -0,0 +1,13 @@
+﻿using Umbraco.Core.Persistence.Mappers;
+
+namespace Umbraco.Core.Models
+{
+    /// <summary>
+    /// Defines a ContentType, which Media is based on
+    /// </summary>
+    [Mapper(typeof(MediaTypeMapper))]
+    public interface IMediaType : IContentTypeComposition
+    {
+         
+    }
+}
