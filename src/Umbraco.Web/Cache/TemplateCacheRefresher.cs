40ad29b3c03076251d80fece17a8fb7c9ae4d365
Morten Christensen <Morten Christensen@localhost>
2013-03-17 18:25:21 +0100

    Initial implementation of the EntityRepository and Service, which will be used to retrieve a basic object with a minimum set of propreties for the models that have their base in the umbracoNode table. The intention is to use this for loading trees in the backoffice for faster loading.

---
+++ src/Umbraco.Web/Cache/TemplateCacheRefresher.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Umbraco.Core;
+using Umbraco.Core.Cache;
+using umbraco;
+using umbraco.interfaces;
+
+namespace Umbraco.Web.Cache
+{
+    public class TemplateCacheRefresher : ICacheRefresher
+    {
+        
+        public string Name
+        {
+            get
+            {
+                return "Template cache refresher";
+            }
+        }
+
+        public Guid UniqueIdentifier
+        {
+            get
+            {
+                return new Guid(DistributedCache.TemplateRefresherId);
+            }
+        }
+
+        public void RefreshAll()
+        {
+        }
+
+        public void Refresh(Guid id)
+        {
+        }
+
+        public void Refresh(int id)
+        {
+            RemoveFromCache(id);
+        }
+
+        public void Remove(int id)
+        {
+            RemoveFromCache(id);
+        }
+
+        private void RemoveFromCache(int id)
+        {
+            ApplicationContext.Current.ApplicationCache.ClearCacheByKeySearch(
+                string.Format("{0}{1}", CacheKeys.TemplateCacheKey, id));
+        }
+
+    }
+}
