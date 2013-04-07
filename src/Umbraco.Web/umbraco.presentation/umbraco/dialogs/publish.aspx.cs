baf6281e0659418d2f37907262df9be7d8ee0419
Morten Christensen <Morten Christensen@localhost>
2013-04-07 20:35:02 +0200

    Making the topological sorter public

---
+++ src/Umbraco.Web/umbraco.presentation/umbraco/dialogs/publish.aspx.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Web;
+using System.Web.SessionState;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+
+using System.Threading;
+using Umbraco.Core.Logging;
+using umbraco.cms.businesslogic.web;
+using umbraco.cms.helpers;
+using umbraco.BasePages;
+
+namespace umbraco.dialogs
+{
+	/// <summary>
+	/// Summary description for publish.
+	/// </summary>
+	public partial class publish : BasePages.UmbracoEnsuredPage
+	{
+		protected System.Web.UI.WebControls.Literal total;
+
+		private int nodeId;
+		private int nodesPublished = 0;
+        public static string pageName = "";
+
+		protected void Page_Load(object sender, System.EventArgs e)
+		{
+			nodeId = int.Parse(helper.Request("id"));
+            cms.businesslogic.web.Document d = new cms.businesslogic.web.Document(nodeId);
+            pageName = d.Text;
+
+			if (d.Level > 1 && d.PathPublished == false)
+			{
+				TheForm.Visible = false;
+				theEnd.Visible = true;
+				feedbackMsg.type = uicontrols.Feedback.feedbacktype.notice;
+				feedbackMsg.Text = ui.Text("publish", "contentPublishedFailedByParent", d.Text, base.getUser()) + "</p><p><a href='#' onClick='" + ClientTools.Scripts.CloseModalWindow() + "'>" + ui.Text("closeThisWindow") + "</a>";
+	               
+				return;
+			}
+
+            // add control prefix to variable for support with masterpages
+            string prefix = PublishUnpublishedItems.ClientID.Replace(PublishUnpublishedItems.ID, "");
+            masterPagePrefix.Text = prefix;
+
+            // by default we only count the published ones
+			int TotalNodesToPublish = cms.businesslogic.web.Document.CountSubs(nodeId, true);
+            try
+            {
+                Application.Lock();
+                // We add both all nodes and only published nodes to the application variables so we can ajax query depending on checkboxes
+                Application["publishTotalAll" + nodeId.ToString()] = cms.businesslogic.CMSNode.CountSubs(nodeId).ToString();
+                Application["publishTotal" + nodeId.ToString()] = TotalNodesToPublish.ToString();
+                Application["publishDone" + nodeId.ToString()] = "0";
+            }
+            finally
+            {
+                Application.UnLock();
+            }
+			total.Text = TotalNodesToPublish.ToString();
+
+			// Put user code to initialize the page here
+			if (!IsPostBack) 
+			{
+				// Add caption to checkbox
+				PublishAll.Text = ui.Text("publish", "publishAll", d.Text, base.getUser());
+				ok.Text = ui.Text("content", "publish", base.getUser());
+				ok.Attributes.Add("style", "width: 60px");
+				ok.Attributes.Add("onClick", "startPublication();");
+
+                // Add checkbox event, so the publish unpublished childs gets enabled
+                PublishUnpublishedItems.LabelAttributes.Add("disabled", "true");
+                PublishUnpublishedItems.LabelAttributes.Add("id", "publishUnpublishedItemsLabel");
+                PublishUnpublishedItems.InputAttributes.Add("disabled", "true");
+                PublishAll.InputAttributes.Add("onclick", "togglePublishingModes(this)");
+			} 
+			else 
+			{
+
+                if (PublishAll.Checked)
+                {
+                    nodesPublished = 0;
+
+                    doPublishSubs(d);
+
+                    Application.Lock();
+                    Application["publishTotal" + nodeId.ToString()] = 0;
+                    Application.UnLock();
+
+                    feedbackMsg.type = umbraco.uicontrols.Feedback.feedbacktype.success;
+
+                    feedbackMsg.Text = ui.Text("publish", "nodePublishAll", d.Text, base.getUser()) + "</p><p><a href='#' onclick='" + ClientTools.Scripts.CloseModalWindow() + "'>" + ui.Text("closeThisWindow") + "</a>";
+
+					ClientTools.ReloadActionNode(true, true);
+
+                    Application.Lock();
+
+                    Application["publishTotal" + nodeId.ToString()] = null;
+                    Application["publishDone" + nodeId.ToString()] = null;
+                    Application.UnLock();
+                }
+                else
+                {
+                    if (d.PublishWithResult(base.getUser()))
+                    {                        
+                        feedbackMsg.type = umbraco.uicontrols.Feedback.feedbacktype.success;
+						feedbackMsg.Text = ui.Text("publish", "nodePublish", d.Text, base.getUser()) + "</p><p><a href='#' onclick='" + ClientTools.Scripts.CloseModalWindow() + "'>" + ui.Text("closeThisWindow") + "</a>";						
+                    }
+                    else {
+                        feedbackMsg.type = umbraco.uicontrols.Feedback.feedbacktype.notice;
+						feedbackMsg.Text = ui.Text("publish", "contentPublishedFailedByEvent", d.Text, base.getUser()) + "</p><p><a href='#' onClick='" + ClientTools.Scripts.CloseModalWindow() + "'>" + ui.Text("closeThisWindow") + "</a>";
+                    }
+					ClientTools.ReloadActionNode(true, false);
+                }
+
+				TheForm.Visible = false;
+                theEnd.Visible = true;
+			}
+		}
+
+		private void doPublishSubs(Document d) 
+		{
+            if (d.Published || PublishUnpublishedItems.Checked)
+            {
+                if (d.PublishWithResult(UmbracoUser))
+                {
+                    
+
+                    nodesPublished++;
+                    Application.Lock();
+                    Application["publishDone" + nodeId.ToString()] = nodesPublished.ToString();
+                    Application.UnLock();
+                    foreach (cms.businesslogic.web.Document dc in d.Children)
+                    {
+                        doPublishSubs(dc);
+                    }
+                }
+                else {
+                    LogHelper.Debug<publish>(string.Format("Publishing node {0} failed due to event cancelling the publishing", d.Id));
+                }
+            }
+		}
+
+        protected override void OnPreRender(EventArgs e) {
+            base.OnPreRender(e);
+
+            ScriptManager.GetCurrent(Page).Services.Add(new ServiceReference("../webservices/publication.asmx"));
+            ScriptManager.GetCurrent(Page).Services.Add(new ServiceReference("../webservices/legacyAjaxCalls.asmx"));
+        }
+
+
+        /// <summary>
+        /// masterPagePrefix control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Literal masterPagePrefix;
+
+        /// <summary>
+        /// JsInclude1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::ClientDependency.Core.Controls.JsInclude JsInclude1;
+
+        /// <summary>
+        /// TheForm control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel TheForm;
+
+        /// <summary>
+        /// PublishAll control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox PublishAll;
+
+        /// <summary>
+        /// PublishUnpublishedItems control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox PublishUnpublishedItems;
+
+        /// <summary>
+        /// ok control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button ok;
+
+        /// <summary>
+        /// ProgBar1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::umbraco.uicontrols.ProgressBar ProgBar1;
+
+        /// <summary>
+        /// theEnd control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel theEnd;
+
+        /// <summary>
+        /// feedbackMsg control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::umbraco.uicontrols.Feedback feedbackMsg;
+	}
+}
---
+++ src/Umbraco.Web/umbraco.presentation/umbraco/webservices/nodeSorter.asmx.cs
@@ -0,0 +1,215 @@
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Web.Script.Services;
+using System.Web.Services;
+using System.Xml;
+using Umbraco.Core.Logging;
+using Umbraco.Core.Persistence.Caching;
+using Umbraco.Web;
+using Umbraco.Web.Security;
+using umbraco.BasePages;
+using umbraco.BusinessLogic.Actions;
+using umbraco.cms.businesslogic.web;
+using Umbraco.Core;
+
+namespace umbraco.presentation.webservices
+{
+    /// <summary>
+    /// Summary description for nodeSorter
+    /// </summary>
+    [WebService(Namespace = "http://umbraco.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [ToolboxItem(false)]
+    [ScriptService]
+    public class nodeSorter : WebService
+    {
+        [WebMethod]
+        public SortNode GetNodes(int ParentId, string App)
+        {
+            if (BasePage.ValidateUserContextID(BasePage.umbracoUserContextID))
+            {
+                var parent = new SortNode { Id = ParentId };
+
+                var nodes = new ArrayList();
+                var cmsNode = new cms.businesslogic.CMSNode(ParentId);
+
+                // Root nodes?
+                if (ParentId == -1)
+                {
+                    if (App == Constants.Applications.Media)
+                    {
+                        foreach (cms.businesslogic.media.Media child in cms.businesslogic.media.Media.GetRootMedias())
+                            nodes.Add(new SortNode(child.Id, child.sortOrder, child.Text, child.CreateDateTime));
+                    }
+                    else
+                        foreach (Document child in Document.GetRootDocuments())
+                            nodes.Add(new SortNode(child.Id, child.sortOrder, child.Text, child.CreateDateTime));
+                }
+                else
+                {
+                    // "hack for stylesheet"
+                    if (App == Constants.Applications.Settings)
+                    {
+                        var styleSheet = new StyleSheet(cmsNode.Id);
+                        foreach (var child in styleSheet.Properties)
+                            nodes.Add(new SortNode(child.Id, child.sortOrder, child.Text, child.CreateDateTime));
+
+                    }
+                    else
+                    {
+                        //store children array here because iterating over an Array property object is very inneficient.
+                        var children = cmsNode.Children;
+                        foreach (cms.businesslogic.CMSNode child in children)
+                            nodes.Add(new SortNode(child.Id, child.sortOrder, child.Text, child.CreateDateTime));
+                    }
+                }
+
+                parent.SortNodes = (SortNode[])nodes.ToArray(typeof(SortNode));
+
+                return parent;
+            }
+
+            throw new ArgumentException("User not logged in");
+        }
+
+        [WebMethod]
+        public void UpdateSortOrder(int ParentId, string SortOrder)
+        {
+            try
+            {
+                if (BasePage.ValidateUserContextID(BasePage.umbracoUserContextID))
+                {
+                    if (SortOrder.Trim().Length > 0)
+                    {
+                        var tmp = SortOrder.Split(',');
+
+                        var isContent = Context.Request.GetItemAsString("app") == Constants.Applications.Content | helper.Request("app") == "";
+                        var isMedia = Context.Request.GetItemAsString("app") == Constants.Applications.Media;
+
+                        for (var i = 0; i < tmp.Length; i++)
+                        {
+                            if (tmp[i] != "" && tmp[i].Trim() != "")
+                            {
+                                if (isContent)
+                                {
+                                    var document = new Document(int.Parse(tmp[i]));
+                                    var published = document.Published;
+                                    document.sortOrder = i;
+                                    document.Save();
+                                    // refresh the xml for the sorting to work
+                                    if (published)
+                                    {
+                                        document.SaveAndPublish(Umbraco.Web.UmbracoContext.Current.UmbracoUser);
+                                        document.refreshXmlSortOrder();
+                                    }
+                                }
+                                // to update the sortorder of the media node in the XML, re-save the node....
+                                else if (isMedia)
+                                {
+                                    var media = new cms.businesslogic.media.Media(int.Parse(tmp[i]));
+                                    media.sortOrder = i;
+                                    media.Save();
+                                }
+                                else
+                                {
+                                    new cms.businesslogic.CMSNode(int.Parse(tmp[i])).sortOrder = i;
+                                }
+                            }
+                        }
+
+                        // Refresh sort order on cached xml
+                        if (isContent)
+                        {
+                            XmlNode parentNode = ParentId == -1 ? content.Instance.XmlContent.DocumentElement : content.Instance.XmlContent.GetElementById(ParentId.ToString());
+
+                            //only try to do the content sort if the the parent node is available... 
+                            if (parentNode != null)
+                                content.SortNodes(ref parentNode);
+
+
+                            // Load balancing - then refresh entire cache
+                            // NOTE: SD: This seems a bit excessive to do simply for sorting! I'm going to leave this here for now but 
+                            //  the sort order should be updated in distributed calls when an item is Published (and it most likely is)
+                            //  but I guess this was put here for a reason at some point.
+                            if (UmbracoSettings.UseDistributedCalls)
+                                library.RefreshContent();
+                        }
+
+                        // fire actionhandler, check for content
+                        if ((helper.Request("app") == Constants.Applications.Content | helper.Request("app") == "") && ParentId > 0)
+                            global::umbraco.BusinessLogic.Actions.Action.RunActionHandlers(new Document(ParentId), ActionSort.Instance);
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                LogHelper.Error<nodeSorter>("An error occurred", ex);
+            }
+
+        }
+    }
+
+    [Serializable]
+    public class SortNode
+    {
+        public SortNode()
+        {
+        }
+
+        private SortNode[] _sortNodes;
+
+        public SortNode[] SortNodes
+        {
+            get { return _sortNodes; }
+            set { _sortNodes = value; }
+        }
+
+        public int TotalNodes
+        {
+            get { return _sortNodes != null ? _sortNodes.Length : 0; }
+            set { int test = value; }
+        }
+        
+        public SortNode(int Id, int SortOrder, string Name, DateTime CreateDate)
+        {
+            _id = Id;
+            _sortOrder = SortOrder;
+            _name = Name;
+            _createDate = CreateDate;
+        }
+
+        private DateTime _createDate;
+
+        public DateTime CreateDate
+        {
+            get { return _createDate; }
+            set { _createDate = value; }
+        }
+        
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        
+        private int _sortOrder;
+
+        public int SortOrder
+        {
+            get { return _sortOrder; }
+            set { _sortOrder = value; }
+        }
+
+        private int _id;
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+    }
+}
